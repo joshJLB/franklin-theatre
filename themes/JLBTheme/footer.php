550fa0aa315da1ba042f8603dad6e44d2bca5f49
unknown <josh@jlbworks.com>
2018-10-03 13:06:44 -0500

    Added footer and mobilized

--- themes/JLBTheme/footer.php
+++ themes/JLBTheme/footer.php
@@ -1,6 +1,31 @@
       <footer class="footer">
-        <section class="footer-top">
-
+        <section class="footer-top" style="background-image: url(<?=home_url(); ?>/wp-content/uploads/2018/10/footer.jpg);">
+          <div class="footer-mission">
+            <img src="<?=home_url(); ?>/wp-content/uploads/2018/10/ft-fan.png" alt="">
+            <h3><?=get_field('mission_title', 'option'); ?></h3>
+            <p><?=get_field('mission_content', 'option'); ?></p>
+          </div>
+          <div class="footer-logo">
+            <img src="<?=get_field('footer_logo', 'option'); ?>" alt="">
+            <div class="footer-socials">
+              <?php if ( have_rows('social_media', 'option') ): ?>
+              <?php while ( have_rows('social_media', 'option') ): the_row(); ?>
+                <a href="<?=get_sub_field('link'); ?>" class="header-socials-links"><i class="fab fa-<?=get_sub_field('platform'); ?>"></i></a>
+              <?php endwhile; ?>
+              <?php endif; ?>
+            </div>
+          </div>
+          <div class="footer-contact">
+            <a href=""><?=get_field('address', 'option'); ?></a>
+            <a href="tel:<?=get_field('phone', 'option'); ?>" class="footer-phone"><?=get_field('phone', 'option'); ?></a>
+            <a href="mailto:<?=get_field('email', 'option'); ?>"><?=get_field('email', 'option'); ?></a>
+          </div>
+          <div class="footer-find-us">
+            <h3>Find Us One</h3>
+            <div class="footer-find-us-two"></div>
+            <img src="<?=get_field('yelp_logo', 'option'); ?>" alt="">
+            <img src="<?=get_field('trip_advisor_logo', 'option'); ?>" alt="">
+          </div>
         </section>
         <section class="jlb">
           <p><a href="http://www.jlbworks.com/#latest-work">Web Design</a>, <a href="http://www.jlbworks.com/#services">Marketing</a> & <a href="http://www.jlbworks.com/#contact">Support</a> by <a href="http://www.jlbworks.com/#latest-work">JLB</a></p>
